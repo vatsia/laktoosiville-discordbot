6526ae1b74273cdec69c6f23aa2bd4be932568ce
Olli Vatsia <olli.vatsia@gmail.com>
2019-05-25 00:42:41 +0300

    bot now fetches foods from sodexo api

--- src/laktoosi.js
+++ src/laktoosi.js
@@ -1,6 +1,9 @@
 var Discord = require("discord.io");
 var logger = require("winston");
+const fetch = require("node-fetch");
+
 var auth = require("./auth.json");
+var botsettings = require("./settings.json");
 
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, 
@@ -34,10 +37,37 @@ bot.on('message', function(user, user_id, channel_id, message, event){
                 })
                 break;
             case 'safkaa':
-                
+                logger.info("TRIGGERING: food");
+                const today = new Date();
+                get_menu_by_date(today.getFullYear(), today.getMonth(), today.getDay(), channel_id);
                 break;
             default:
                 break;
         }
     }
 })
+
+const get_menu_by_date = function(year, month, day, channel){
+    const url = botsettings.food_api_url + "" + botsettings.food_restaurant_id + "/" + year + "/" + month + "/" + day + "/" + botsettings.food_language;
+    logger.info("FETCHING:" + url);
+    fetch(url)
+        .then( res => res.json())
+        .then(json => {
+            foods = json.courses
+            if(foods.length > 0){
+                foods.forEach(element => {
+                    bot.sendMessage({
+                        to: channel,
+                        message: element.title_fi
+                    })
+                })
+            }else{
+                bot.sendMessage({
+                    to: channel,
+                    message: "Janille ei enää tarjoilla"
+                })
+            }
+
+        })
+    
+}
